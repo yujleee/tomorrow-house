11d02d3cadfcaffa15c26eccd800c9301dce2c77
이유정 <yujlee242@gmail.com>
2022-06-21 11:55:22 +0900

    🚧  order-cta 토글 추가

---
+++ js/order-cta.js
@@ -0,0 +1,19 @@
+const orderCta = document.querySelector('.order-cta');
+const [bookmarkButton, buyButton] = orderCta.children;
+
+const orderModal = document.querySelector('.order-form-modal');
+const orderModalOverlay = document.querySelector('.overlay');
+
+function openOrderModal() {
+  orderModal.classList.add('is-open');
+  orderModalOverlay.classList.add('is-active');
+}
+
+function closeOrderModal() {
+  orderModal.classList.remove('is-open');
+  orderModalOverlay.classList.remove('is-active');
+}
+
+buyButton.addEventListener('click', openOrderModal);
+
+orderModalOverlay.addEventListener('click', closeOrderModal);
