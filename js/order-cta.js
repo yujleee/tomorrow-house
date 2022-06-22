6d850e87905408183c2f9047ebc2f0fda296a114
이유정 <yujlee242@gmail.com>
2022-06-22 17:46:28 +0900

    🚧  북마크 버튼 토글 추가

--- js/order-cta.js
+++ js/order-cta.js
@@ -14,5 +14,26 @@ function closeOrderModal() {
   orderModalOverlay.classList.remove('is-active');
 }
 
+function toggleBookmark() {
+  const [icon, countSpan] = this.children;
+  const count = Number(countSpan.innerHTML.replace(',', ''));
+
+  let newCount = count;
+
+  if (this.classList.contains('is-active')) {
+    icon.setAttribute('class', 'ic-bookmark');
+    newCount -= 1;
+  } else {
+    icon.setAttribute('class', 'ic-bookmark-filled');
+    newCount += 1;
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString();
+  countSpan.setAttribute('aria-lable', `북마크 ${newCount.toLocaleString()}회`);
+  this.classList.toggle('is-active');
+}
+
 buyButton.addEventListener('click', openOrderModal);
 orderModalOverlay.addEventListener('click', closeOrderModal);
+
+bookmarkButton.addEventListener('click', toggleBookmark);
