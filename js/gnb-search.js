f76f8f081c3ce06a1591df09f21d2a71f1820a0c
이유정 <yujlee242@gmail.com>
2022-06-23 10:38:03 +0900

    🚧  검색내역 전체 삭제 기능 추가

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,17 @@
 const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbSearchHistory = gnbSearch.querySelector('.search-history');
+const gnbSearchHistoryList = gnbSearchHistory.querySelector(
+  '.search-history-list'
+);
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+);
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) return;
+
   gnbSearchHistory.classList.add('is-active');
   window.addEventListener('click', closeGnbSearchHistory);
 }
@@ -14,4 +23,10 @@ function closeGnbSearchHistory(e) {
   }
 }
 
+function deleteAllHistoryItems() {
+  gnbSearchHistoryList.innerHTML = '';
+  gnbSearchHistory.classList.remove('is-active');
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+deleteAllButton.addEventListener('click', deleteAllHistoryItems);
