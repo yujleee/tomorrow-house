9cab375bb77af4619e6c12dca08129ef5cd0d130
이유정 <yujlee242@gmail.com>
2022-06-23 11:07:11 +0900

    🚧  검색 내역 각각 삭제될 수 있도록 구현

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -8,6 +8,13 @@ const gnbSearchHistoryList = gnbSearchHistory.querySelector(
 const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 );
+const deleteButtonList =
+  gnbSearchHistoryList.querySelectorAll('.button-delete');
+
+function closeGnbSearch() {
+  gnbSearchHistory.classList.remove('is-active');
+  window.removeEventListener('click', closeGnbSearchHistory);
+}
 
 function openGnbSearchHistory() {
   if (gnbSearchHistoryList.children.length === 0) return;
@@ -17,16 +24,25 @@ function openGnbSearchHistory() {
 }
 
 function closeGnbSearchHistory(e) {
-  if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active');
-    window.removeEventListener('click', closeGnbSearchHistory);
-  }
+  if (!gnbSearch.contains(e.target)) closeGnbSearch();
 }
 
 function deleteAllHistoryItems() {
   gnbSearchHistoryList.innerHTML = '';
-  gnbSearchHistory.classList.remove('is-active');
+  closeGnbSearch();
+}
+
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation();
+  const itemToDelete = this.parentNode;
+  gnbSearchHistoryList.removeChild(itemToDelete);
+
+  if (gnbSearchHistoryList.children.length === 0) closeGnbSearch();
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
 deleteAllButton.addEventListener('click', deleteAllHistoryItems);
+
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHistoryItem);
+});
