e5fbc70b75e8256a3c369836f703bbbbe0c7293d
이유정 <yujlee242@gmail.com>
2022-06-24 10:57:45 +0900

    💫  product-tab 클릭시 해당 패널로 움직이는 스크롤 이벤트 추가

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,5 +1,7 @@
 const productTab = document.querySelector('.product-tab');
 const productTabList = productTab.querySelectorAll('button');
+const TOP_HEADER_DESKTOP = 80 + 50 + 54;
+const TOP_HEADER_MOBILE = 50 + 40 + 40;
 
 let currentActiveTab = productTab.querySelector('.is-active');
 
@@ -13,4 +15,21 @@ function activeTab() {
   }
 }
 
-productTabList.forEach((button) => button.addEventListener('click', activeTab));
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby');
+  const tabPanel = document.querySelector(`#${tabPanelId}`);
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE);
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  });
+}
+
+productTabList.forEach((button) => {
+  button.addEventListener('click', activeTab);
+  button.addEventListener('click', scrollToTabPanel);
+});
