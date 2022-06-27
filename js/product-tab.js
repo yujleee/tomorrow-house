053a2433f5b054c1a5965bf1d68389a125cf4b2a
이유정 <yujlee242@gmail.com>
2022-06-27 11:17:13 +0900

    🐛  스크롤 이벤트 핸들러의 버그들 수정

--- js/product-tab.js
+++ js/product-tab.js
@@ -4,14 +4,20 @@ const TOP_HEADER_DESKTOP = 80 + 50 + 54;
 const TOP_HEADER_MOBILE = 50 + 40 + 40;
 
 let currentActiveTab = productTab.querySelector('.is-active');
+let disableUpdating = false;
 
 function activeTab() {
   const tabItem = this.parentNode;
 
   if (currentActiveTab !== tabItem) {
+    disableUpdating = true;
     tabItem.classList.add('is-active');
     currentActiveTab.classList.remove('is-active');
     currentActiveTab = tabItem;
+
+    setTimeout(() => {
+      disableUpdating = false;
+    }, 1000);
   }
 }
 
@@ -55,12 +61,16 @@ function detectTabPanelPosition() {
     const position = window.scrollY + panel.getBoundingClientRect().top;
     productTabPanelPositionMap[id] = position;
   });
+
+  updateActiveTabOnScroll();
 }
 
 function updateActiveTabOnScroll() {
   // 스크롤 위치에 따라서 active tab 업데이트
   // 1. 현재 유저가 얼만큼 스크롤을 했는지? => window.scrollY
   // 2. 각 tabPanel의 y축 위치 => productTabPanelPositionMap
+
+  if (disableUpdating) return;
 
   const scrolledAmount =
     window.scrollY +
@@ -81,12 +91,25 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabList[0];
   }
 
+  // 페이지 끝까지 스크롤한 경우
+  // window.innerWidth < 1200 -> orderCTA 존재. margin-bottom 56px 추가됨
+  const bodyHeight =
+    document.body.offsetHeight + (window.innerWidth < 1200 ? 56 : 0);
+
+  if (window.scrollY + window.innerHeight === bodyHeight) {
+    newActiveTab = productTabList[4];
+  }
+
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode;
 
     if (newActiveTab !== currentActiveTab) {
       newActiveTab.classList.add('is-active');
-      currentActiveTab.classList.remove('is-active');
+
+      if (currentActiveTab !== null) {
+        currentActiveTab.classList.remove('is-active');
+      }
+
       currentActiveTab = newActiveTab;
     }
   }
